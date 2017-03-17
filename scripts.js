a76286e5f9d9f1815d632eb3a9cfed33ea0fffa2
Abhinav <thakareabhinav@gmail.com>
2017-03-17 15:14:35 +0530

    initial commits

---
+++ scripts.js
@@ -0,0 +1,43 @@
+$(function() {
+    var bingo = {
+        selectedNumbers: [],
+        generateRandom: function() {
+            var min = 1;
+            var max = 90;
+            var random = Math.floor(Math.random() * (max - min + 1)) + min;
+            return random;
+        },
+        generateNextRandom: function() {
+            if (bingo.selectedNumbers.length > 89) {
+                alert("All numbers Exhausted");
+                $('.bigNumberDisplay span').text("90");
+                $('td.cell' + 0).addClass('selected');
+                return 0;
+            }
+            var random = bingo.generateRandom();
+            while ($.inArray(random, bingo.selectedNumbers) > -1) {
+                random = bingo.generateRandom();
+            }
+            bingo.selectedNumbers.push(random);
+            return random;
+        }
+    };
+    $('td').each(function() {
+        var concatClass = this.cellIndex + "" + this.parentNode.rowIndex;
+        var numberString = parseInt(concatClass, 10).toString();
+        $(this).addClass("cell" + numberString).text(numberString);
+    });
+    $('#btnGenerate').click(function() {
+        var random = bingo.generateNextRandom().toString();
+        $('.bigNumberDisplay span').text(random);
+        $('td.cell' + random).addClass('selected');
+    });
+    window.onbeforeunload = function(e) {
+        e = e || window.event;
+        var returnString = 'Are you sure?';
+        if (e) {
+            e.returnValue = returnString;
+        }
+        return returnString;
+    };
+});
